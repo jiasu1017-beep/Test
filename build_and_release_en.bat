5d607c1fb22eca1a1194450136c2b69719d2bba9
jiasu <45795504+Haiezan@users.noreply.github.com>
2026-03-02 17:02:52 +0800

    chore: 更新版本号至v0.0.7并添加发布说明
    更新应用程序版本号及相关文件中的版本引用，并添加v0.0.7版本的详细发布说明文档

--- build_and_release_en.bat
+++ build_and_release_en.bat
@@ -5,7 +5,7 @@ REM Switch to script directory
 cd /d "%~dp0"
 
 REM Set version and release directory
-set VERSION=v0.0.6
+set VERSION=v0.0.7
 set RELEASE_DIR=release-%VERSION%
 
 echo ========================================
